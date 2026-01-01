c864e9ed0f2a009919aa9dc8f5a7c49f2dc98976
Khushbu Gaur <103189526+Khushh03@users.noreply.github.com>
2026-01-01 09:02:08 +0000

    feat: implement contact page with form and FAQ section
    - Added a new contact page with a form for user inquiries.
    - Integrated Accordion component for FAQ section.
    - Updated button, input, and textarea components for consistent styling.
    - Enhanced route tree to include the new contact route.
    - Introduced Navbar in the root layout for better navigation.
    - Refactored existing components for improved readability and maintainability.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router"
 import HeroSection from "./-components/sections/hero"
 import AboutSection from "./-components/sections/AboutSection"
-import Navbar from "./-components/sections/Navbar"
-import OurImpactSection from './-components/sections/OurImpactSection';
+import OurImpactSection from "./-components/sections/OurImpactSection"
 import OurCourses from "./-components/sections/OurCourses"
 import Help from "./-components/sections/Help"
-import ProgramsSection from "./-components/sections/ProgramsSection";
+import ProgramsSection from "./-components/sections/ProgramsSection"
 export const Route = createFileRoute("/")({
     component: RouteComponent,
 })
 
 function RouteComponent() {
     return (
-        <div>
-            <Navbar />
-            <main>
-                <HeroSection />
-                <AboutSection />
-                <OurImpactSection />
-                <OurCourses />
-                <ProgramsSection />
-                <Help />
-            </main>
-        </div>
+        <main>
+            <HeroSection />
+            <AboutSection />
+            <OurImpactSection />
+            <OurCourses />
+            <ProgramsSection />
+            <Help />
+        </main>
     )
 }
